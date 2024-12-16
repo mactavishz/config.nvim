b0b5ed997c3fd472b68f85bd53f028751e0309b8
Chao Zhan <chao.zhan@campus.tu-berlin.de>
2024-12-16 14:37:45 +0100

    add mason-confirm

---
+++ lua/custom/plugins/mason-confirm.lua
@@ -0,0 +1,9 @@
+return {
+  -- automatically install formatters defined in conform
+  'zapling/mason-conform.nvim',
+  dependencies = {
+    'williamboman/mason.nvim',
+    'stevearc/conform.nvim',
+  },
+  opts = {},
+}
