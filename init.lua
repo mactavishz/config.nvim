f992781b225e6531e7370f51e2a1adff38ee0e79
Chao Zhan <chao.zhan@campus.tu-berlin.de>
2024-12-20 12:22:40 +0100

    use space space to find existing buffers

--- init.lua
+++ init.lua
@@ -191,7 +191,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
       vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
       vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[F]ind existing buffers' })
+      vim.keymap.set('n', '<space><space>', builtin.buffers, { desc = '[F]ind existing buffers' })
       vim.keymap.set('n', '<leader>fp', function()
         vim.cmd [[ Telescope luasnip ]]
       end, { desc = '[F]ind existing snippets' })
@@ -250,7 +250,7 @@ require('lazy').setup({
       { 'j-hui/fidget.nvim', opts = {} },
 
       -- Allows extra capabilities provided by nvim-cmp
-      -- 'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lsp',
     },
     config = function(_, opts)
       --  This function gets run when an LSP attaches to a particular buffer.
@@ -362,10 +362,6 @@ require('lazy').setup({
       --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      -- capabilities.textDocument.foldingRange = {
-      --   dynamicRegistration = false,
-      --   lineFoldingOnly = true,
-      -- }
       -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
       -- Enable the following language servers
@@ -422,14 +418,6 @@ require('lazy').setup({
         ansiblels = {},
       }
 
-      -- local lspconfig = require 'lspconfig
-      -- for server, config in pairs(servers) do
-      --   -- passing config.capabilities to blink.cmp merges with the capabilities in your
-      --   -- `opts[server].capabilities, if you've defined it
-      --   config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-      --   lspconfig[server].setup(config)
-      -- end
-
       -- configure filetype
       vim.filetype.add { pattern = { ['.*%.ansible%..*'] = 'yaml.ansible' } }
 
@@ -448,144 +436,14 @@ require('lazy').setup({
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            local blink_capabilities = require('blink.cmp').get_lsp_capabilities(server.capabilities)
-
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, blink_capabilities or {})
+            -- local blink_capabilities = require('blink.cmp').get_lsp_capabilities(server.capabilities)
+            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities)
             require('lspconfig')[server_name].setup(server)
           end,
         },
       }
     end,
   },
-
-  -- { -- Autocompletion
-  --   'hrsh7th/nvim-cmp',
-  --   event = 'InsertEnter',
-  --   dependencies = {
-  --     -- Snippet Engine & its associated nvim-cmp source
-  --     {
-  --       'L3MON4D3/LuaSnip',
-  --       build = (function()
-  --         -- Build Step is needed for regex support in snippets.
-  --         -- This step is not supported in many windows environments.
-  --         -- Remove the below condition to re-enable on windows.
-  --         if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-  --           return
-  --         end
-  --         return 'make install_jsregexp'
-  --       end)(),
-  --       dependencies = {
-  --         -- `friendly-snippets` contains a variety of premade snippets.
-  --         --    See the README about individual language/framework/plugin snippets:
-  --         --    https://github.com/rafamadriz/friendly-snippets
-  --         {
-  --           'rafamadriz/friendly-snippets',
-  --           config = function()
-  --             require('luasnip.loaders.from_vscode').lazy_load()
-  --           end,
-  --         },
-  --       },
-  --     },
-  --     'saadparwaiz1/cmp_luasnip',
-  --     'hrsh7th/cmp-nvim-lsp',
-  --     'hrsh7th/cmp-path',
-  --   },
-  --   config = function()
-  --     -- See `:help cmp`
-  --     local cmp = require 'cmp'
-  --     local luasnip = require 'luasnip'
-  --     luasnip.config.setup {
-  --       history = true,
-  --       updateevents = 'TextChanged,TextChangedI',
-  --     }
-  --
-  --     -- Think of <c-l> as moving to the right of your snippet expansion.
-  --     --  So if you have a snippet that's like:
-  --     --  function $name($args)
-  --     --    $body
-  --     --  end
-  --     --
-  --     -- <c-l> will move you to the right of each of the expansion locations.
-  --     -- <c-h> is similar, except moving you backwards.
-  --
-  --     -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-  --     --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-  --     vim.keymap.set({ 'i', 's' }, '<C-l>', function()
-  --       if luasnip.expand_or_locally_jumpable() then
-  --         luasnip.expand_or_jump()
-  --       end
-  --     end, { silent = true })
-  --
-  --     vim.keymap.set({ 'i', 's' }, '<C-h>', function()
-  --       if luasnip.locally_jumpable(-1) then
-  --         luasnip.jump(-1)
-  --       end
-  --     end, { silent = true })
-  --
-  --     vim.keymap.set({ 'i', 's' }, '<C-k>', function()
-  --       if luasnip.choice_active() then
-  --         luasnip.change_choice(1)
-  --       end
-  --     end, { silent = true })
-  --
-  --     cmp.setup {
-  --       snippet = {
-  --         expand = function(args)
-  --           luasnip.lsp_expand(args.body)
-  --         end,
-  --       },
-  --       completion = { completeopt = 'menu,menuone,noinsert' },
-  --
-  --       -- For an understanding of why these mappings were
-  --       -- chosen, you will need to read `:help ins-completion`
-  --       --
-  --       -- No, but seriously. Please read `:help ins-completion`, it is really good!
-  --       mapping = cmp.mapping.preset.insert {
-  --         -- Select the [n]ext item
-  --         ['<C-n>'] = cmp.mapping.select_next_item(),
-  --         -- Select the [p]revious item
-  --         ['<C-p>'] = cmp.mapping.select_prev_item(),
-  --
-  --         -- Scroll the documentation window [b]ack / [f]orward
-  --         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-  --         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  --
-  --         -- Accept ([y]es) the completion.
-  --         --  This will auto-import if your LSP supports it.
-  --         --  This will expand snippets if the LSP sent a snippet.
-  --         ['<C-y>'] = cmp.mapping.confirm { select = true },
-  --
-  --         -- If you prefer more traditional completion keymaps,
-  --         -- you can uncomment the following lines
-  --         --['<CR>'] = cmp.mapping.confirm { select = true },
-  --         --['<Tab>'] = cmp.mapping.select_next_item(),
-  --         --['<S-Tab>'] = cmp.mapping.select_prev_item(),
-  --
-  --         -- Manually trigger a completion from nvim-cmp.
-  --         --  Generally you don't need this, because nvim-cmp will display
-  --         --  completions whenever it has completion options available.
-  --         ['<C-Space>'] = cmp.mapping.complete {},
-  --       },
-  --       sources = {
-  --         {
-  --           name = 'lazydev',
-  --           -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-  --           group_index = 0,
-  --         },
-  --         -- Copilot Source
-  --         { name = 'nvim_lsp' },
-  --         { name = 'luasnip' },
-  --         { name = 'copilot' },
-  --         { name = 'path' },
-  --       },
-  --       window = {
-  --         documentation = {
-  --           border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
-  --         },
-  --       },
-  --     }
-  --   end,
-  -- },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
@@ -608,6 +466,8 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      -- Highlight the word under the cursor, for better 'iw' text object selection
+      require('mini.cursorword').setup()
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
