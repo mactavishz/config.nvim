1397f4a89dbccebde3f3fded117fe25fdcd14853
mactavishz <maczhanchao@yahoo.com>
2025-05-08 12:15:21 +0200

    refactor: always display all diagnostics

--- lua/custom/plugins/inline-diagnostic.lua
+++ lua/custom/plugins/inline-diagnostic.lua
@@ -6,6 +6,18 @@ return {
     opts = {
       preset = 'classic', -- "classic" or "modern"
       transparent_bg = true,
+      options = {
+        use_icons_from_diagnostic = true,
+        throttle = 0,
+        multiple_diag_under_cursor = true,
+        multilines = {
+          enabled = true,
+          always_show = true,
+        },
+        show_all_diags_on_cursorline = false,
+        enable_on_insert = true,
+        enable_on_select = true,
+      },
     },
     config = function(_, opts)
       require('tiny-inline-diagnostic').setup(opts)
