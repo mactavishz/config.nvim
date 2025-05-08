d44ebc38bf76b0f633890476c92bf4bd746f498e
mactavishz <maczhanchao@yahoo.com>
2025-05-08 12:15:34 +0200

    feat: add treesitter-textobjects

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -3,6 +3,9 @@ return {
     'nvim-treesitter/nvim-treesitter-context',
   },
   {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
+  {
     'windwp/nvim-ts-autotag',
     event = 'VeryLazy',
     opts = {},
